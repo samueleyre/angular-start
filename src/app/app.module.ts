3ba7e9db52d8935fded666e67d294b59beef88dc
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-12-08 10:53:35 +0100

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestComponent } from './test/test.component';
-import { MyPageComponent } from './my-page/my-page.component';
-import { MySecondPageComponent } from './my-second-page/my-second-page.component';
-import { MySecondPageOneComponent } from './my-second-page-one/my-second-page-one.component';
-import { MySecondPageTwoComponent } from './my-second-page-two/my-second-page-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    MyPageComponent,
-    MySecondPageComponent,
-    MySecondPageOneComponent,
-    MySecondPageTwoComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import { User } from '../entities/user';
+import { environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  signin(email: string, password: string): Observable<any> {
+    return this.httpClient.post(
+      `${environment.api}/api/login_check`,
+      {email, password}
+      );
+  }
+
+  signup(user: User): Observable<any> {
+    return this.httpClient.post(
+      `${environment.api}/api/signup`,
+      user
+    );
+  }
+
+  me(): Observable<any> {
+    return this.httpClient.get(
+      `${environment.api}/api/ping`
+    );
+  }
+
+}
