8b77ffd7412496cb871cf5939e5d9cd3985dbdf8
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-12-10 15:20:01 +0100

    reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {MyFirstPageComponent} from './my-first-page/my-first-page.component';
-import {MySecondPageComponent} from './my-second-page/my-second-page.component';
-import {MySecondPageOneComponent} from './my-second-page-one/my-second-page-one.component';
-import {MySecondPageTwoComponent} from './my-second-page-two/my-second-page-two.component';
-
 const routes: Routes = [
-  {
-    path: 'myFirstPage', component: MyFirstPageComponent
-  },
-  {
-    path: 'mySecondPage',
-    // component: MySecondPageComponent,
-    children: [
-      {
-        path: 'one',
-        component: MySecondPageOneComponent,
-      },
-      {
-        path: 'two',
-        component: MySecondPageTwoComponent,
-      }
-    ]
-  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MyNameComponent } from './my-name/my-name.component';
-import { MyFirstPageComponent } from './my-first-page/my-first-page.component';
-import { MySecondPageComponent } from './my-second-page/my-second-page.component';
-import { MySecondPageOneComponent } from './my-second-page-one/my-second-page-one.component';
-import { MySecondPageTwoComponent } from './my-second-page-two/my-second-page-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNameComponent,
-    MyFirstPageComponent,
-    MySecondPageComponent,
-    MySecondPageOneComponent,
-    MySecondPageTwoComponent
   ],
   imports: [
     BrowserModule,
