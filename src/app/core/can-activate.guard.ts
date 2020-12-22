bf3914a0ca0c86a8ab5f89efc6aba954cdbc8a91
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-12-22 10:01:39 +0100

    guard

--- src/app/core/can-activate.guard.ts
+++ src/app/core/can-activate.guard.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {AuthService} from './services/auth.service';
-import {SessionService} from './services/session.service';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,6 @@ export class CanActivateGuard implements CanActivate {
 
   constructor(
     private authService: AuthService,
-    private sessionService: SessionService
   ) {
   }
 
@@ -19,10 +18,40 @@ export class CanActivateGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
+    return this.authService.me().pipe(
 
+      // on regarde si on a un erreur de permission et on retourne le status de la requête
+      catchError((error: Response) => {
+        let status = 500;
+        if (error.status === 401 || error.status === 403) { // unauthorized or forbidden //
+          status = error.status;
+        }
+        return of({ status });
+      }),
 
-
-    return true;
+      // on retourne true ou false en fonction du status ( et donc de la permission )
+      map((response: Response) => {
+        return !(401 === response.status || 403 === response.status);
+      })
+    );
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
