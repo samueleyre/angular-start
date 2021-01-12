b3fc5d8a18f3aa58632aae0eebb37b2f74018079
Samuel Eyre <samuel.eyre@hotmail.fr>
2021-01-12 09:58:32 +0100

    add profile page

--- src/app/dash/dash.module.ts
+++ src/app/dash/dash.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {DashRoutingModule} from './dash-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import {MatCardModule} from '@angular/material';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ProfileComponent],
   imports: [
     CommonModule,
     DashRoutingModule,
