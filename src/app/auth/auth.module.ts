69ceab63babb9b764c76af1dd56f8710e6a0a640
Samuel Eyre <samuel.eyre@hotmail.fr>
2021-01-13 20:48:58 +0100

    use sharedmodule to clean modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,22 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninComponent } from './components/signin/signin.component';
 import {AuthRoutingModule} from './auth-routing.module';
-import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
-import {ReactiveFormsModule} from '@angular/forms';
 import { SignupComponent } from './components/signup/signup.component';
 import {UserPartsModule} from '../shared/user-parts/user-parts.module';
+import {SharedModule} from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     AuthRoutingModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
     UserPartsModule
   ]
 })
--- src/app/dash/dash.module.ts
+++ src/app/dash/dash.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {DashRoutingModule} from './dash-routing.module';
 import { HomeComponent } from './components/home/home.component';
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule} from '@angular/material';
 import { ProfileComponent } from './components/profile/profile.component';
-import {ReactiveFormsModule} from '@angular/forms';
 import {UserPartsModule} from '../shared/user-parts/user-parts.module';
+import {SharedModule} from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [HomeComponent, ProfileComponent],
   imports: [
-    CommonModule,
     DashRoutingModule,
-    MatCardModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    UserPartsModule
+    UserPartsModule,
+    SharedModule
   ]
 })
 export class DashModule { }
